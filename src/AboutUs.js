e5365e8a83bc4d165303d3e9b0d4b1dbd32e360a
Kevin Henry <kevinhenry@live.com>
2021-05-15 19:08:59 -0700

    Create AboutUs.js

---
+++ src/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class AboutUs extends React.Component {
+  render() {
+    return(
+      <>
+      <h2>Hello, About us</h2>
+      </>
+    );
+  }
+}
+export default AboutUs;
