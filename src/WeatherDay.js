bc7835f05e16f0c3cb389e12aa7383380f30b70b
Adrian Milsap <adrian@hirobius.com>
2021-05-17 21:27:54 -0700

    auto render Weather and Joke API working

--- src/WeatherDay.js
+++ src/WeatherDay.js
@@ -1,33 +1,23 @@
 import React from 'react';
 
-// Weather data for static display
-
-// link to API
-
-// link to Database
-
-// Bootstrap Required
-
-
 class WeatherDay extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      city: '',
+      cityData: {},
+      weatherData: [],
+    }
   }
 
   render() {
     console.log(this.props.weatherData);
-    return(
+    return (
       <>
-        <h6>Description: {this.props.weatherData.description}</h6>
-        {/* <p>Date: {this.props.weatherData.date}</p> */}
-        <p>Clouds: {this.props.weatherData.clouds}</p>
-        {/* <p>{this.props.weatherData.}</p> */}
-        <p>Temp: {this.props.weatherData.temp}°C</p>
-        <p>Wind Direction: {this.props.weatherData.directionString} -{this.props.weatherData.directionDegree}</p>
-        {/* <div style={"color:blue;"}>!</div> */}
-        {/* <h1 style="color:blue;">A Blue Heading</h1> */}
+        <p>{this.props.weatherData.description}</p>
+        <p>{this.props.weatherData.clouds}</p>
+        <p>{this.props.weatherData.temp}</p>
       </>
     )
   }
