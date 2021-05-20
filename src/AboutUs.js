bbee4c41b9dc8a8011934a3c8acf57390400ffa3
Adrian Milsap <adrian@hirobius.com>
2021-05-19 20:16:54 -0700

    AboutUs edits

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import './aboutUs.css';
 import NavBar from './Profile';
+import Member from './Member';
+import CardDeck from 'react-bootstrap/CardDeck';
 
 class AboutUs extends React.Component {
   render() {
     return (
       <>
-        <h2>Hello, About us</h2>
         <NavBar />
+        <h1 className="title">About us</h1>
+        <CardDeck>
+          <Member
+            name="Adrian Milsap"
+            imgSrc="public/adrian.png"
+            bio="Software engineer with a UX Design background. Super nerd looking for full-time work."
+            linkedIn="https://www.linkedin.com/in/adrianmilsap/"
+            gitHub="https://github.com/hirobius"
+          />
+          <Member
+            name="Anthony Williams"
+            imgSrc="./public/anthony.jpg"
+            bio="Student at CodeFellows Software Development | Systems Administrator | Red Hat Linux Administrator | DoD Contractor | Software Developer"
+            linkedIn="https://www.linkedin.com/in/anthonyjwilliams15/"
+            gitHub="https://github.com/Williamsjanthony15"
+          />
+          <Member
+            name="Kassie Bradshaw"
+            imgSrc="./public/kassie.jpg"
+            bio="Emergent software developer specializing in Python."
+            linkedIn="https://www.linkedin.com/in/kassie-bradshaw-2021"
+            gitHub="https://github.com/kassiebradshaw"
+          />
+          <Member
+            name="Kevin Henry"
+            imgSrc="./public/kevin.jpg"
+            bio="I'm a Developer with a passion for solving problems for people. I've been in people operations for the last few years and I'm looking to make a shift into a more technical role with a larger impact. I want to help build something that empowers people around the world."
+            linkedIn="http://linkedin.com/in/skhenry"
+            gitHub="https://github.com/kevinhenry"
+          />
+        </CardDeck>
       </>
     );
   }
